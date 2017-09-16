ef4e71f06c17288ea66a4f655bbcbb70f52c5cf1
adeseye sijuwade <adeseyesijuwade@gmail.com>
2017-09-16 18:43:49 -0400

    add auth route

--- BarHoppersApp/app.js
+++ BarHoppersApp/app.js
@@ -13,27 +13,28 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(
-  session({
+app.use(session({
     key: process.env.SECRET_KEY,
     secret: process.env.SECRET_KEY,
     resave: false,
     saveUninitialized: true,
-  }),
-);
+  }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static('public'));
-
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
 
+//Routes
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
+
+const authRoutes = require('./routes/auth-routes');
+app.use('/auth', authRoutes);
 
 app.use('*', (req, res) => {
   res.status(400).json({
