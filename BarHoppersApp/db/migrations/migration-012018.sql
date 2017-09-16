0204836f516e623fab6dd523a4a5d64e39e74b98
adeseye sijuwade <adeseyesijuwade@gmail.com>
2017-09-16 17:31:18 -0400

    add migrations

---
+++ BarHoppersApp/db/migrations/migration-012018.sql
@@ -0,0 +1,23 @@
+\c bar_hoppers_dev;
+
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  firstname VARCHAR(255) NOT NULL,
+  lastname VARCHAR(255) NOT NULL,
+  username VARCHAR(255) UNIQUE NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password_digest TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS bars (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  address VARCHAR(255),
+  city VARCHAR(255),
+  phone VARCHAR(255),
+  website VARCHAR(255),
+  hours VARCHAR(255),
+  type VARCHAR(255),
+  image TEXT,
+  user_id INT REFERENCES users(id)
+);
