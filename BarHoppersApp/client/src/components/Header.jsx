5087c20d252339b35dcb7d71499e573e23d8ec00
adeseye sijuwade <adeseyesijuwade@gmail.com>
2017-09-16 13:51:36 -0400

    add header component

---
+++ BarHoppersApp/client/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = (props) => {
+  return(
+    <header className="header">
+          <div className="navBar">
+            <ul>
+              <h1>Bar Hoppers</h1>
+              <li><h3>REGISTER</h3></li>
+              <li><h3>SIGN IN</h3></li>
+              <li><h3>HOME</h3></li>
+            </ul>
+          </div>
+    </header>
+  )
+}
+
+export default Header;
