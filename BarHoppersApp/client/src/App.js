91e1081d0fde41265f94a2a3a1314aeb9948681c
adeseye sijuwade <adeseyesijuwade@gmail.com>
2017-09-16 20:56:16 -0400

    add auth functionality

--- BarHoppersApp/client/src/App.js
+++ BarHoppersApp/client/src/App.js
@@ -1,15 +1,84 @@
 import React, { Component } from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
+import axios from 'axios';
 import Header from './components/Header';
 import Homepage from './components/Homepage';
+import Login from './components/Login';
+import Register from './components/Register';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      auth: false,
+      user: null,
+    }
+    this.handleLoginSubmit = this.handleLoginSubmit.bind(this);
+    this.handleRegisterSubmit = this.handleRegisterSubmit.bind(this);
+    this.logOut = this.logOut.bind(this);
+  }
+
+//Auth
+  handleLoginSubmit(e, username, password) {
+    e.preventDefault();
+    axios.post('/auth/login', {
+      username,
+      password,
+    }).then(res => {
+      this.setState({
+        auth: res.data.auth,
+        user: res.data.user,
+      });
+     }).catch(err => console.log(err));
+  }
+
+  handleRegisterSubmit(e, firstname, lastname, username, email, password) {
+    e.preventDefault();
+    axios.post('/auth/register', {
+      firstname,
+      lastname,
+      username,
+      email,
+      password,
+    }).then(res => {
+      this.setState({
+        auth: res.data.auth,
+        user: res.data.user,
+      });
+    }).catch(err => console.log(err));
+  }
+
+  logOut() {
+    axios.get('/auth/logout')
+      .then(res => {
+        this.setState({
+          auth: false,
+        });
+      }).catch(err => console.log(err));
+  }
+
+//Render
   render() {
     return (
+     <Router>
       <div className="App">
-        <Header />
-        <Homepage />
+        <Header
+          logOut={this.logOut}
+        />
+        <Route exact path='/' render={() => <Homepage
+          /> } />
+        <Route exact path='/login' render={() => <Login
+          handleLoginSubmit={this.handleLoginSubmit}
+          /> } />
+        <Route exact path='/register' render={() => <Register
+          handleRegisterSubmit={this.handleRegisterSubmit}
+          /> } />
       </div>
+     </Router>
     );
   }
 }
