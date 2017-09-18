69cff669c485525364203c1d9088b87567841f3d
adeseye sijuwade <adeseyesijuwade@gmail.com>
2017-09-18 13:32:20 -0400

    add auth to header component

--- BarHoppersApp/client/src/components/Header.jsx
+++ BarHoppersApp/client/src/components/Header.jsx
@@ -7,18 +7,18 @@ const Header = (props) => {
           <div className="navBar">
             <ul>
               <h1>Bar Hoppers</h1>
-              <li onClick={props.logOut}>
-                <Link to='/'><h3>logout</h3></Link>
-              </li>
-              <li>
-                <NavLink exact to='/register'><h3>register</h3></NavLink>
-              </li>
-              <li>
-                <NavLink exact to='/login'><h3>login</h3></NavLink>
-              </li>
-              <li>
-                <NavLink exact to='/profile'><h3>profile</h3></NavLink>
-              </li>
+              {props.auth
+                ? <li onClick={props.logOut}><Link to='/'><h3>logout</h3></Link></li>
+                : ''}
+              {!props.auth
+                ? <li><NavLink exact to='/register'><h3>register</h3></NavLink></li>
+                : ''}
+              {!props.auth
+                ? <li><NavLink exact to='/login'><h3>login</h3></NavLink></li>
+                : ''}
+              {props.auth
+                ?<li><NavLink exact to='/profile'><h3>profile</h3></NavLink></li>
+                : ''}
               <li>
                 <NavLink exact to='/'><h3>home</h3></NavLink>
               </li>
