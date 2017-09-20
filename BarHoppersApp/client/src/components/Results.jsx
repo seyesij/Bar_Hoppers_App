cf4426cec4627cf430470f00c65266fe960ce6e7
adeseye sijuwade <adeseyesijuwade@gmail.com>
2017-09-19 21:53:53 -0400

    add link to single page

--- BarHoppersApp/client/src/components/Results.jsx
+++ BarHoppersApp/client/src/components/Results.jsx
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class Results extends Component {
   render(){
     return(
       <div className="bars">
        <div className="barimage">
-        <img className="barimages" src={this.props.searchResults.Bar_Image} alt=""></img>
-        </div>
+        <Link to='/bar'>
+          <img onClick={() => this.props.getBarData(this.props.searchResults.Name, this.props.searchResults.Zip)} src={this.props.searchResults.Bar_Image}  className="barimages" alt=""></img>
+        </Link>
+       </div>
+
         <div className="bardetail">
           <h2>{this.props.searchResults.Name}</h2>
           <p className="barcity">{this.props.searchResults.City}</p>
