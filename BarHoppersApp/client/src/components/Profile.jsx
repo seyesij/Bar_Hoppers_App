35145d1ac2df1d8867102ca1a1ec44e8f393d39d
adeseye sijuwade <adeseyesijuwade@gmail.com>
2017-09-20 11:09:43 -0400

    add user data/functions to profile

--- BarHoppersApp/client/src/components/Profile.jsx
+++ BarHoppersApp/client/src/components/Profile.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
 
 const Profile = (props) => {
-  return(
-    <div className="profile">
-      <h1 className="profileheading">{props.user.username}'s Profile</h1>
+  if(props.usersBars) {
+    return (
+      <div className="profilecontainer1">
+        <h2>Your Favorite Bars!</h2>
+        <div className="profilecontainer2">
+          <div className="profile">
+            {props.usersBars.map(bar => {
+              return (
+                <div className='bar'>
+                 <h3>{bar.name}</h3>
+                 <img key={bar.id} src={bar.image} alt={bar.name} id={bar.id}></img>
+                 <br></br>
+                 <div className="favinfo">
+                    <p>{bar.city}</p>
+                    <button className="favButton" onClick={() => props.removeFavorite(bar.id)}>Unfavorite</button>
+                 </div>
+                </div>
+              )
+            })}
+        </div>
+      </div>
     </div>
-  )
+    )
+  } else {
+    return <h1>Loading...</h1>;
+  }
 }
 
 export default Profile;
+
