ff1d355f19bf919bb2128e4f94f72961c3c93793
adeseye sijuwade <adeseyesijuwade@gmail.com>
2017-10-19 15:05:40 -0400

    enable cors

--- app.js
+++ app.js
@@ -23,6 +23,12 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'client/build')));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
